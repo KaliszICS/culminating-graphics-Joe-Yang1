aa5880ecab3183312e0f2af054f5be95cba3e469
Joe-Yang1 <348993072@tdsb.ca>
2026-06-08 06:11:51 +0000

    Prog

--- Game.java
+++ Game.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -28,9 +29,21 @@ public class Game extends Application {
 
         menu = new Scene(menuSpacing, 900, 600);
 
+
         Label settingPage = new Label("Options");
+        settingPage. setStyle("-fx-font-size: 50;");
+
+        CheckBox tipsToggled = new CheckBox("Display tips");
+
+        Button returButton = new Button("Return");
+
+        VBox settingOptions = new VBox(25);
+        settingOptions.getChildren().addAll( settingPage, tipsToggled, returButton);
+
+        setting = new Scene(settingOptions, 900, 600);
 
         
+
     }
     
     
